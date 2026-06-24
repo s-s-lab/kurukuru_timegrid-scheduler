8005577d08c30c945d9a5c53ae32d658835989c8
s-s-lab <shige.s.j@gmail.com>
2026-06-25 08:28:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TIMEGRID_CONFIG = {
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/XXXXXXXXXXXX/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwmRdnl0VQ7OY1rRtD7mkheufEKeNSowtCuzTKKA8hjxTXP389DLDt-6vw6Tw0YIWsQEQ/exec',
   FRONTEND_BASE_URL: window.location.origin + window.location.pathname.replace(/\/[^/]*$/, '')
 };
