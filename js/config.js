0bf083866f911eaeb68ee731f2534d5b73a6564c
s-s-lab <shige.s.j@gmail.com>
2026-06-25 12:27:49 +0900

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-window.TIMEGRID_CONFIG = {
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbx1twS2du0YBNsMkQIfBIZZSB5XBV7NU6qupWXO2Doi3LuNsTSWUdeRFIVyVllgSxXodw/exec',
-  FRONTEND_BASE_URL: window.location.origin + window.location.pathname.replace(/\/[^/]*$/, '')
-};
