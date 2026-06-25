22bc7bb2650695a7cdd17d060ffda864479f8876
s-s-lab <shige.s.j@gmail.com>
2026-06-25 12:28:01 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.TIMEGRID_CONFIG = {
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw8NDdIoZrfnNVHkHMssZmvNCJIBxBe_HOpDcw3RaTLhUR509gEXWFrjd0_j_Tgv5oATw/exec',
+  FRONTEND_BASE_URL: window.location.origin + window.location.pathname.replace(/\/[^/]*$/, '')
+};
